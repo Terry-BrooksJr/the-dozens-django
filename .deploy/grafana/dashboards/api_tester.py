19c70f7f164b7d4aafaa32b403eeba13b8416e01
Terry A. Brooks, Jr. <Terry.Arthur@BrooksJr.com>
2026-04-20 12:47:26 -0400

    fix: clean up formatting and improve readability in api_tester.py

--- .deploy/grafana/dashboards/api_tester.py
+++ .deploy/grafana/dashboards/api_tester.py
@@ -7,24 +7,27 @@ class ApiRequest:
     def __init__(self, base_url, user_token):
         self._base_url = base_url
         self._token = user_token
-    
-    def get_token(self) -> str: 
+
+    def get_token(self) -> str:
         return f"Token {self._token}"
-    
-    def get_url(self) -> str: 
+
+    def get_url(self) -> str:
         return self._base_url
-    
-    def set_headers(self) -> dict: 
-        return {
-            "Authorization": self.get_token()
-        }
-        
+
+    def set_headers(self) -> dict:
+        return {"Authorization": self.get_token()}
+
     def test_endpoint(self, category, nsfw):
-       return requests.get(url=f"{self.get_url}/api/{category}", params={"nsfw": nsfw}, headers=self.set_headers())
-        
-        
-        
+        return requests.get(
+            url=f"{self.get_url}/api/{category}",
+            params={"nsfw": nsfw},
+            headers=self.set_headers(),
+        )
+
+
 if __name__ == "__main__":
-    tester = ApiRequest("https://api.yo-momma.io/","b31ec6397d8b3c8f959f5b6150f27c47bc02acec" )
+    tester = ApiRequest(
+        "https://api.yo-momma.io/", "b31ec6397d8b3c8f959f5b6150f27c47bc02acec"
+    )
     for case in TEST_CASES:
         tester.test_endpoint(category=case["category"], nsfw=case["nsfw"])
