3084acb3bab925ace95eee9a22c417edd7125879
Terry A. Brooks, Jr. <Terry.Arthur@BrooksJr.com>
2026-06-05 18:00:40 -0400

    fix: update Grafana dashboard URL in admin view

--- core/admin_view.py
+++ core/admin_view.py
@@ -9,7 +9,7 @@ def grafana_dashboard_view(request):
         **admin.site.each_context(request),
         "title": "Observability Dashboard",
         "grafana_url": (
-            "https://brooksjr.grafana.net/public-dashboards/53837066b22a437db5eb980adfe484bc"
+            "https://grafana.yo-momma.io/public-dashboards/3691683c85b749c989b9f3339b52a600"
         ),
     }
     return TemplateResponse(request, "admin/grafana_dashboard.html", context)
