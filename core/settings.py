a715f85ce3b2fe29725b1f696f6b0abd474c413b
Terry Brooks <Terry.Arthur@BrooksJr.com>
2026-09-04 02:26:23 -0500

    ci: update environment variable names for S3 compatibility in Taskfile and settings

--- core/settings.py
+++ core/settings.py
@@ -507,10 +507,10 @@ class Base(Configuration):
         environ=True, environ_prefix=None, environ_name="S3_OBJECT_STORAGE_KEY"
     )
     AWS_SECRET_ACCESS_KEY = values.SecretValue(
-        environ=True, environ_prefix=None, environ_name="DO_SPACES_SECRET"
+        environ=True, environ_prefix=None, environ_name="S3_OBJECT_STORAGE_SECRET"
     )
     AWS_STORAGE_BUCKET_NAME = values.SecretValue(
-        environ=True, environ_prefix=None, environ_name="DO_SPACES_BUCKET"
+        environ=True, environ_prefix=None, environ_name="S3_OBJECT_STORAGE_BUCKET_NAME"
     )
     AWS_S3_ENDPOINT_URL = values.SecretValue(
         environ=True, environ_prefix=None, environ_name="S3_ENDPOINT_URL"
